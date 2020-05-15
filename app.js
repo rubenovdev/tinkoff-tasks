c9ac7002ca0b7f6936ebc24a782c5d5bc2bdf6ee
Alexander Rubenov <alexander.rubenov@gmail.com>
2020-05-15 18:20:02 +0300

    finished lesson-3

--- app.js
+++ app.js
@@ -1,6 +1,50 @@
 'use strict'
 
+const isPromise = (obj) => {
+  return (
+    !!obj &&
+    (typeof obj === 'object' || typeof obj === 'function') &&
+    typeof obj.then === 'function'
+  )
+}
+
+const promisify = (fn) => {
+  return (...args) => {
+    try {
+      const result = fn(...args)
+
+      return CustomPromise.resolve(result)
+    } catch (error) {
+      return CustomPromise.reject(error)
+    }
+  }
+}
+
 class CustomPromise {
+  static resolve(value) {
+    return new CustomPromise((resolve) => {
+      if (isPromise(value)) {
+        value.then((data) => {
+          resolve(data)
+        })
+      } else {
+        resolve(value)
+      }
+    })
+  }
+
+  static reject(error) {
+    return new CustomPromise((_, reject) => {
+      if (isPromise(error)) {
+        error.then((data) => {
+          reject(data)
+        })
+      } else {
+        reject(error)
+      }
+    })
+  }
+
   constructor(executor) {
     try {
       executor(this.resolve, this.reject)
@@ -13,40 +57,64 @@ class CustomPromise {
   result = undefined
   stack = []
 
-  resolve = (value) => {
-    this.status = 'resolved'
+  changeParameters(status, value) {
+    if (this.status !== 'pending') {
+      return
+    }
+
+    this.status = status
     this.result = value
 
     this.stack.forEach((fn) => fn())
   }
 
-  reject = (error) => {
-    this.status = 'rejected'
-    this.result = error
+  resolve = (value) => {
+    this.changeParameters('resolved', value)
+  }
 
-    this.stack.forEach((fn) => fn())
+  reject = (error) => {
+    this.changeParameters('rejected', error)
+  }
+
+  checkStatus(fn) {
+    if (this.status === 'pending') {
+      this.stack.push(fn)
+    } else {
+      fn()
+    }
   }
 
   then(onResolved, onRejected) {
+    const normalizedOnResolved = promisify(onResolved)
+    const normalizedOnRejected = onRejected && promisify(onRejected)
+
     return new CustomPromise((resolve, reject) => {
       const check = () => {
         if (this.status === 'resolved') {
           try {
-            resolve(onResolved(this.result))
+            const result = normalizedOnResolved(this.result)
+
+            result.then((data) => {
+              resolve(data)
+            })
           } catch (error) {
             reject(error)
           }
         }
         if (this.status === 'rejected') {
-          onRejected ? resolve(onRejected(this.result)) : reject(this.result)
+          if (normalizedOnRejected) {
+            const result = normalizedOnRejected(this.result)
+
+            result.then((data) => {
+              resolve(data)
+            })
+          } else {
+            reject(this.result)
+          }
         }
       }
 
-      if (this.status === 'pending') {
-        this.stack.push(check)
-      } else {
-        check()
-      }
+      this.checkStatus(check)
     })
   }
 
@@ -64,26 +132,28 @@ class CustomPromise {
         }
       }
 
-      if (this.status === 'pending') {
-        this.stack.push(check)
-      } else {
-        check()
-      }
+      this.checkStatus(check)
     })
   }
 }
 
 const promise = new CustomPromise((resolve, reject) => {
-  setTimeout(() => resolve('value in executor'), 100)
+  setTimeout(() => {
+    resolve('value in executor')
+    resolve('value in executor2')
+  }, 100)
 })
   .then(() => {
     console.log('hey 111')
+    return new CustomPromise((resolve) => resolve('first value'))
   })
-  .then(() => {
+  .then((value) => {
+    console.log(value)
     console.log('hey 222')
-    throw new Error('блин, ошибка во втором then..')
+    return 'heeeeeeey'
   })
-  .then(() => {
+  .then((value) => {
+    console.log(value)
     console.log('hey 333')
   })
   .catch((error) => {
@@ -98,24 +168,3 @@ const promise = new CustomPromise((resolve, reject) => {
   })
 
 console.log(promise)
-
-// const customPromise = new CustomPromise((resolve, reject) => {
-//   setTimeout(() => reject(new Error('Whoops1!')), 1000)
-// })
-//   .then(() => {
-//     console.log('hey 111')
-//   })
-//   .then(() => {
-//     console.log('hey 222')
-//   })
-//   .then(() => {
-//     console.log('hey 333')
-//   })
-//   .catch((error) => {
-//     console.log('решили ошибку')
-//   })
-//   .then(() => {
-//     console.log('hey 444')
-//   })
-
-// console.log(customPromise)
