89b273aac279a57cc00c3642b7685bb66f770757
Alexander Rubenov <alexander.rubenov@gmail.com>
2020-05-15 15:55:00 +0300

    added catch

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-"use strict"
+'use strict'
 
 class CustomPromise {
   constructor(executor) {
-    executor(this.resolve, this.reject)
+    try {
+      executor(this.resolve, this.reject)
+    } catch (error) {
+      this.reject(error)
+    }
   }
 
-  status = "pending"
+  status = 'pending'
   result = undefined
   stack = []
 
   resolve = (value) => {
-    this.status = "resolved"
+    this.status = 'resolved'
     this.result = value
 
     this.stack.forEach((fn) => fn())
   }
 
   reject = (error) => {
-    this.status = "rejected"
+    this.status = 'rejected'
     this.result = error
 
     this.stack.forEach((fn) => fn())
@@ -26,15 +30,19 @@ class CustomPromise {
   then(onResolved, onRejected) {
     return new CustomPromise((resolve, reject) => {
       const check = () => {
-        if (this.status === "resolved") {
-          resolve(onResolved(this.result))
+        if (this.status === 'resolved') {
+          try {
+            resolve(onResolved(this.result))
+          } catch (error) {
+            reject(error)
+          }
         }
-        if (this.status === "rejected") {
+        if (this.status === 'rejected') {
           onRejected ? resolve(onRejected(this.result)) : reject(this.result)
         }
       }
 
-      if (this.status === "pending") {
+      if (this.status === 'pending') {
         this.stack.push(check)
       } else {
         check()
@@ -42,30 +50,72 @@ class CustomPromise {
     })
   }
 
-  // catch() {}
+  catch(onRejected) {
+    return new CustomPromise((resolve, reject) => {
+      const check = () => {
+        if (this.status === 'rejected') {
+          try {
+            resolve(onRejected(this.result))
+          } catch (error) {
+            reject(error)
+          }
+        } else if (this.status === 'resolved') {
+          resolve(this.result)
+        }
+      }
+
+      if (this.status === 'pending') {
+        this.stack.push(check)
+      } else {
+        check()
+      }
+    })
+  }
 }
 
-const first = new CustomPromise((resolve, reject) => {
-  setTimeout(() => reject("fail2"), 0)
+const promise = new CustomPromise((resolve, reject) => {
+  setTimeout(() => resolve('value in executor'), 100)
 })
+  .then(() => {
+    console.log('hey 111')
+  })
+  .then(() => {
+    console.log('hey 222')
+    throw new Error('блин, ошибка во втором then..')
+  })
+  .then(() => {
+    console.log('hey 333')
+  })
+  .catch((error) => {
+    console.log('зашли в первый catch')
+    throw new Error('вызвали новую ошибку')
+  })
+  .then(() => {
+    console.log('hey 444')
+  })
+  .catch((error) => {
+    console.log('решили ошибку во втором catch')
+  })
 
-const customPromise = first
-  .then((result) => console.log("fd", result))
-  .then(
-    () => undefined,
-    (error) => console.log("error2: ", error)
-  )
-  .then((result) => console.log("new result", result))
+console.log(promise)
 
-const customPromise2 = first
-  .then(
-    (result) => console.log("fd2", result),
-    (error) => (console.log("error: ", error), "result")
-  )
-  .then(
-    (res) => console.log(res),
-    (res) => console.log("still error", res)
-  )
+// const customPromise = new CustomPromise((resolve, reject) => {
+//   setTimeout(() => reject(new Error('Whoops1!')), 1000)
+// })
+//   .then(() => {
+//     console.log('hey 111')
+//   })
+//   .then(() => {
+//     console.log('hey 222')
+//   })
+//   .then(() => {
+//     console.log('hey 333')
+//   })
+//   .catch((error) => {
+//     console.log('решили ошибку')
+//   })
+//   .then(() => {
+//     console.log('hey 444')
+//   })
 
-console.log(customPromise)
-console.log(customPromise2)
+// console.log(customPromise)
