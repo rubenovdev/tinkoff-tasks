62ed61a51f6457877d32c31da7d6dc5db466acb2
Alexander Rubenov <alexander.rubenov@gmail.com>
2020-07-17 17:25:50 +0300

    Задачи на замыкания

--- this.js
+++ this.js
@@ -65,8 +65,87 @@
 
 // ========== Задача 4
 
-const sum = () => {}
+// const sum = (a) => {
+//   let currentSum = 0
 
-sum() // 0
-sum(1)(2)() // 3
-sum(1)(2)(3)() // 6
+//   function innerSum(b) {
+//     if (b === undefined) {
+//       return currentSum
+//     }
+
+//     currentSum += b
+//     return innerSum
+//   }
+
+//   innerSum.valueOf = () => currentSum
+
+//   return innerSum(a)
+// }
+
+// // console.log(sum) // 0
+// // console.log(sum(1)(2) + 2) // 5
+// // console.log(sum(1)(2)(3)) // 6
+
+// ========== Задача 5
+
+// function plus(a, b, c, d) {
+//   return a + b + c + d
+// }
+
+// function curry(fn) {
+//   const argsLength = fn.length
+
+//   return function innerFn(...newArgs) {
+//     if (newArgs.length >= argsLength) {
+//       return fn(...newArgs)
+//     }
+
+//     return innerFn.bind(undefined, ...newArgs)
+//   }
+// }
+
+// const curryPlus = curry(plus)
+
+// const plusFive = curryPlus(5)
+// console.log(plusFive(4, 3, 5)) // 17
+// console.log(plusFive(10, 1)(3)) // 19
+// console.log(curryPlus(1, 2)(2)(4)) // 9
+
+// ========== Задача 6
+
+// const compareArrays = (arr1, arr2) => {
+//   return (
+//     arr1 &&
+//     arr2 &&
+//     arr1.length === arr2.length &&
+//     arr1.every((elem, index) => elem === arr2[index])
+//   )
+// }
+
+// const memoize = (fn) => {
+//   let currentArgs = null
+//   let fnResult
+
+//   return (...args) => {
+//     if (!compareArrays(currentArgs, args)) {
+//       fnResult = fn(...args)
+//       currentArgs = args
+//     }
+
+//     return fnResult
+//   }
+// }
+
+// const plus = memoize((a, b) => {
+//   console.log('зашли в plus')
+//   return a + b
+// })
+
+// console.log(plus(1, 2))
+// console.log(plus(1, 2))
+// console.log(plus(2, 3))
+
+// const alwaysFive = memoize(() => 5)
+
+// console.log(alwaysFive())
+
